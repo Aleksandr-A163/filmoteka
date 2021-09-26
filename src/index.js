46d4da463d388e2b2c49b6c9274a68fb5cc4c0c6
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2021-09-26 21:16:13 +0800

    добавляет базовый класс js
    1) добавляет базовый класс js
    2) добавляет плагин handlebars
    3) добавляет начальный рендер коллекции

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './sass/main.scss';
+import FetchApi from "./partialsJS/apiFetch";
+import render from "./templates/render.hbs"
+
+// элемент списка
+const ulEl = document.getElementById('root');
+
+// создаёт новый класс на основе базового
+const newFetchApi = new FetchApi();
+
+// запись в локалсторедж объектом
+newFetchApi.fetchGenres().then(r => localStorage.setItem('genres', JSON.stringify(r.genres)));
+
+// пример того что сохранилось в локалсторедж
+// console.log(JSON.parse(localStorage.getItem('genres')))
+
+// запись в localeStorage отдельными id и name - побаловаться)
+// newFetchApi.fetchGenres().then(r => {r.genres.forEach(e => localStorage.setItem(`${e.id}`,  e.name))})
+
+// запрос за популярными фильмами за день и рендер
+newFetchApi.fetchApi().then(results => {
+    renderFile(results)
+})
+
+// функция рендера
+function renderFile(results) {
+    ulEl.insertAdjacentHTML('beforeend', render({results}))
+}
