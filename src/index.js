2fccd6bfe8438fd94a58a06c51c33e1fab59cf2b
Anna Vasileva <anette.vasileva@gmail.com>
2021-09-27 16:43:14 +0300

    добавляет поиск по ключевому слову
    минимально работающая версия поиска

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './sass/main.scss';
-import FetchApi from "./JSpart/apiFetch";
-import render from "./templates/render.hbs"
+import FetchApi from './JSpart/apiFetch';
+import render from './templates/render.hbs';
+// import foundFilmsByKeyword from './search-by-keyword';
 
 // элемент списка
 const ulEl = document.getElementById('root');
+
+// элементы поиска по ключевому слову
+const inputSearchFilm = document.querySelector('.search__input');
+const btnSearchEl = document.querySelector('.search__button');
 
 // создаёт новый класс на основе базового
 const newFetchApi = new FetchApi();
@@ -12,17 +17,36 @@ const newFetchApi = new FetchApi();
 newFetchApi.fetchGenres().then(r => localStorage.setItem('genres', JSON.stringify(r.genres)));
 
 // пример того что сохранилось в локалсторедж
-// console.log(JSON.parse(localStorage.getItem('genres')))
+// console.log(JSON.parse(localStorage.getItem('genres')));
 
 // запись в localeStorage отдельными id и name - побаловаться)
 // newFetchApi.fetchGenres().then(r => {r.genres.forEach(e => localStorage.setItem(`${e.id}`,  e.name))})
 
 // запрос за популярными фильмами за день и рендер
 newFetchApi.fetchApi().then(results => {
-    renderFile(results)
-})
+  renderFile(results);
+});
 
 // функция рендера
 function renderFile(results) {
-    ulEl.insertAdjacentHTML('beforeend', render({results}))
+  ulEl.insertAdjacentHTML('beforeend', render({ results }));
+}
+
+//поиск по названию фильма
+btnSearchEl.addEventListener('click', foundFilmsByKeyword);
+
+//функция поиска по названию фильма
+function foundFilmsByKeyword(e) {
+  e.preventDefault();
+  const inputSearchEl = e.target.closest('.search').querySelector('.search__input');
+  const query = inputSearchEl.value.trim();
+
+  if (!query) {
+    return;
+  }
+  newFetchApi.query = query;
+  newFetchApi.fetchSearchFilms().then(film => {
+    console.log(film);
+    renderFile(film);
+  });
 }
