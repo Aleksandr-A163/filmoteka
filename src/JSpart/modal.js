5750581e673291db2522a35d232ac5a31184548c
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2021-09-28 18:10:19 +0800

    изменения с localeStorage
    1) внесено добавление текущей коллекции в localeStorage
    2) добавление динамических данных в модалку Ирины))
    3) добавление фильмов в localeStorage Queue

--- src/JSpart/modal.js
+++ src/JSpart/modal.js
@@ -1,21 +1,54 @@
-const ulEl = document.getElementById('root');
-// ulEl взят из index.js
+import modal from '../templates/modal.hbs'
+
+const collectionList = document.getElementById('home');
+// collectionList взят из index.js
+const modalContent = document.querySelector('.modal__content');
+
 const btnModalCloseEl = document.querySelector('.button-modal--close');
 const backdropEl = document.querySelector('.backdrop');
 const modalImageEl = document.querySelector('.modal__image');
 
-ulEl.addEventListener('click', onUlElClick);
+collectionList.addEventListener('click', onUlElClick);
+// переменная для фильма, который открыт в модальном окне
+let curFilm;
 
 function onUlElClick(e) {
     e.preventDefault();
+    console.log(e.target)
     // if (!e.target.classList.contains('card')) {
     // return
     // }
     window.addEventListener('keydown', onKeyPress);
     backdropEl.classList.remove('is-hidden');
-    console.log(e.target.src);
-    modalImageEl.src = e.target.src;
-  modalImageEl.alt = e.target.alt;
+//     console.log(e.target.src);
+//     modalImageEl.src = e.target.src;
+//   modalImageEl.alt = e.target.alt;
+    
+    //работа с хранилищем
+  const currentArrayFilms = JSON.parse(localStorage.getItem('currentFilms'));
+  const currentTarget = e.target.id;
+  curFilm = currentArrayFilms.find(elem => elem.id === Number(currentTarget));
+    renderModal(curFilm);
+    const currentQueue = JSON.parse(localStorage.getItem('queue'));
+    // console.log(currentQueue)
+    //условие проверки нахождения фильма в хранилище
+    const buttonQueueEl = document.getElementById('queueInModal');
+    if (!currentQueue.some(e => e.id === Number(currentTarget))) {
+        buttonQueueEl.addEventListener('click', addToQueue);
+    }  
+}
+
+// Функция добавления фильма в localeStorage Oueue
+function addToQueue() {
+  const currentQueue = localStorage.getItem('queue');
+  const NextQueue = JSON.parse(currentQueue);
+  NextQueue.push(curFilm);
+  localStorage.setItem('queue', JSON.stringify(NextQueue));
+//   console.log(NextQueue);
+}
+// функция динамического рендера разметки модалки с данными
+function renderModal(curFilm) {
+  modalContent.innerHTML = modal({ curFilm });
 }
 
 function onKeyPress(event) {
