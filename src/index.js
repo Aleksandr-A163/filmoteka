0184c2f70f3e2f7e79105c2e9e521f6291ebaaab
MikhailTsyganov <tsyganov.mik@gmail.com>
2021-09-29 21:30:33 +0300

    header-btn-fix

--- src/index.js
+++ src/index.js
@@ -1,60 +1,29 @@
 import './sass/main.scss';
 import FetchApi from "./JSpart/apiFetch";
-import render from "./templates/card.hbs";
+import render from "./templates/render.hbs"
+import './JSpart/header-setup';
 
 // элемент списка
-const collectionList = document.getElementById('home');
-
-// элементы поиска по ключевому слову
-const inputSearchFilm = document.querySelector('.search__input');
-const btnSearchEl = document.querySelector('.search__button');
+const ulEl = document.getElementById('root');
 
 // создаёт новый класс на основе базового
 const newFetchApi = new FetchApi();
 
-// запись в локалсторедж всех жанров
+// запись в локалсторедж объектом
 newFetchApi.fetchGenres().then(r => localStorage.setItem('genres', JSON.stringify(r.genres)));
-// запись в локалсторедж зарендеренных фильмов
-newFetchApi.fetchApi().then(r => localStorage.setItem('currentFilms', JSON.stringify(r)));
-//функция проверки наличия в "очереди" фильмов и создания массива если нету
-function isGetQueue() {
-  if (localStorage.getItem('queue')) return;
-  localStorage.setItem('queue', '[]');
-}
-isGetQueue();
 
-//функция проверки наличия в "просмотренных" фильмов и создания массива если нету
-function isGetWatched() {
-  if (localStorage.getItem('watched')) return;
-  localStorage.setItem('watched', '[]');
-}
-isGetWatched();
+// пример того что сохранилось в локалсторедж
+// console.log(JSON.parse(localStorage.getItem('genres')))
+
+// запись в localeStorage отдельными id и name - побаловаться)
+// newFetchApi.fetchGenres().then(r => {r.genres.forEach(e => localStorage.setItem(`${e.id}`,  e.name))})
 
 // запрос за популярными фильмами за день и рендер
 newFetchApi.fetchApi().then(results => {
-  renderFile(results);
-});
+    renderFile(results)
+})
 
 // функция рендера
 function renderFile(results) {
-  collectionList.insertAdjacentHTML('beforeend', render({ results }));
-}
-
-//поиск по названию фильма
-btnSearchEl.addEventListener('click', foundFilmsByKeyword);
-
-//функция поиска по названию фильма
-function foundFilmsByKeyword(e) {
-  e.preventDefault();
-  const inputSearchEl = e.target.closest('.search').querySelector('.search__input');
-  const query = inputSearchEl.value.trim();
-
-  if (!query) {
-    return;
-  }
-  newFetchApi.query = query;
-  newFetchApi.fetchSearchFilms().then(film => {
-    console.log(film);
-    renderFile(film);
-  });
+    ulEl.insertAdjacentHTML('beforeend', render({results}))
 }
