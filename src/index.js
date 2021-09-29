8f8091271086e5d9fb48742ab45c008d506bcb0d
Tagir Chanyshev <chanyshev.tagir@gmail.com>
2021-09-29 19:15:37 +0300

    добавление модального окна со студентами

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './sass/main.scss';
 import FetchApi from "./JSpart/apiFetch";
 import render from "./templates/render.hbs"
+import "./JSpart/modal_students";
 
 // элемент списка
 const ulEl = document.getElementById('root');
