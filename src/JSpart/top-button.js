9f79b8aaed9deb65f3b24b6451f0ba009daf6b60
Aleksandr-A163 <quark_31@mail.ru>
2021-10-07 20:12:45 +0400

    вывел кнопку
    1) Есть баг при переходе в библиотеку, кнопка выводится сразу, т.к виден футер

--- src/JSpart/top-button.js
+++ src/JSpart/top-button.js
@@ -1,22 +1,21 @@
-// We select the element we want to target
+// Назначаю константы для отслеживания
 const target = document.querySelector("footer");
 const scrollToTopBtn = document.querySelector(".scrollToTopBtn");
 const rootElement = document.documentElement;
 
-// Next we want to create a function that will be called when that element is intersected
+
 function callback(entries, observer) {
-  // The callback will return an array of entries, even if you are only observing a single item
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      // Show button
       scrollToTopBtn.classList.add("showBtn");
     } else {
-      // Hide button
+
       scrollToTopBtn.classList.remove("showBtn");
     }
   });
 }
 
+// Функция скрола наверх страницы
 function scrollToTop() {
   rootElement.scrollTo({
     top: 0,
@@ -27,6 +26,6 @@ scrollToTopBtn.addEventListener("click", scrollToTop);
 
 // Next we instantiate the observer with the function we created above. This takes an optional configuration
 // object that we will use in the other examples.
-let observer = new IntersectionObserver(callback);
+const observer = new IntersectionObserver(callback);
 // Finally start observing the target element
 observer.observe(target);
